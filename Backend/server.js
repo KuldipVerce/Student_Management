fda82162af8929327f96b188961f1a831a67d285
KuldipVerce <kpanditpawar@gmail.com>
2025-06-02 17:22:33 +0530

    final test2

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,9 @@
 //assign dependencies to variables.
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
 const app = express();
 dotenv.config();
 
@@ -13,11 +13,11 @@ const PORT = process.env.PORT || 8070;
 //app.use(cors());
 
 app.use(
-  cors({
-    origin: ['https://student-management-tau-ecru.vercel.app'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-  })
+	cors({
+		origin: ["https://student-management-tau-ecru.vercel.app"],
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		credentials: true,
+	})
 );
 
 /* Add the bodyParser middleware to parse JSON requests. will extract the JSON data from
@@ -29,9 +29,9 @@ app.use(bodyParser.json());
 const URL = process.env.MONGODB_URL;
 
 mongoose.connect(URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useNewUrlParser: true,
 });
 // .then(() => {
 //     console.log('Connected to database!');
@@ -44,20 +44,20 @@ mongoose.connect(URL, {
 
 //once connected to the databse this will use to check it
 const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log('Mongodb Connection Success!');
+connection.once("open", () => {
+	console.log("Mongodb Connection Success!");
 });
 
 //create coonnection between front end and backend
-const studentRoute = require('./routes/students_route');
+const studentRoute = require("./routes/students_route");
 
 /*By using app.use() method with the '/student' route as the first argument,
  you are telling Express to use this middleware for any route that starts with '/student'.
   This means that if a client makes a request to /student/create, /student/view, /student/update, or
   any other route that starts with '/student', the request will be passed on to the studetRoute middleware
    to handle.*/
-app.use('/student', studentRoute);
+app.use("/student", studentRoute);
 
 app.listen(PORT, () => {
-  console.log(`Server is up and running on port : ${PORT}`);
+	console.log(`Server is up and running on port : ${PORT}`);
 });
